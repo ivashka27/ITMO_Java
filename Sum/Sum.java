d8f3da7658f203d08ecad23e9f35ccd5ce24bbb7
ivashka27 <41290137+ivashka27@users.noreply.github.com>
2021-09-10 13:55:07 +0300

    whithout regex

--- Sum/Sum.java
+++ Sum/Sum.java
@@ -3,14 +3,23 @@ public class Sum {
         int result = 0;
         int N = args.length;
         for (String str : args) {
-            String regex = "\\p{javaWhitespace}";
-            String curr = str.replaceAll(regex, " ").strip();
-            String[] processed_nums = curr.split(" ");
-            for (String elem : processed_nums) {
-                if (elem.length() == 0) {
-                    continue;
+            int pos_first = -1;
+            int len = str.length();
+            for (int pos_second = 0; pos_second < len; pos_second++) {
+                if (!Character.isWhitespace(str.charAt(pos_second))) {
+                    if (pos_first == -1) {
+                        pos_first = pos_second;
+                    }
+                } else if (pos_first != -1) {
+                    String curr = str.substring(pos_first, pos_second);
+                    int num = Integer.parseInt(curr);
+                    result += num;
+                    pos_first = -1;
                 }
-                int num = new Integer(elem);
+            }
+            if (pos_first != -1) {
+                String curr = str.substring(pos_first);
+                int num = Integer.parseInt(curr);
                 result += num;
             }
         }
